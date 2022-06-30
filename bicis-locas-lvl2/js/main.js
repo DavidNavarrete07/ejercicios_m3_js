d8067e556a1c5b3c50a002e5267138d5e8263948
DavidNavarrete07 <davidalejandro20-09@outlook.es>
2022-06-30 12:27:51 -0400

    Modificación validaciones js a ejercicio de bicis locas

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -1,68 +1,81 @@
-	const name = document.querySelector("#name");
-	let first_word_name = name.value.trim();
-	const last_name = document.querySelector("#lastname");
-	let first_word_lastname = last_name.value.trim();
-	const email = document.querySelector("#input-email");
-	const password = document.querySelector("#input-password");
-	const type_bike = document.querySelector("select");
-	const patternA_Z = /^[A-Za-z]+$/;
+
+function validateForm() {
+	const name_input = document.querySelector("#name").value.trim();
+	const last_name = document.querySelector("#lastname").value.trim();
+	const email = document.querySelector("#input-email").value.trim();
+	const password = document.querySelector("#input-password").value.trim();
+	const type_bike = document.querySelector("select").value;
+	const patternA_Z = /[a-zA-Z]/;
 	const pattern_email = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-
-	function validateForm() {
-
-		// Validacion nombre
-		if (name.value.trim() === "") {
-			document.querySelector(".name-container").insertAdjacentHTML("beforeend", "<span>Campo obligatorio</span>");
-		} else if (!name.value.match(patternA_Z)) {
-			document.querySelector(".name-container span").remove();
-			document.querySelector(".name-container").insertAdjacentHTML("beforeend", "<span>Solo se permiten letras [A-Z]</span>");
-		} else if (first_word_name.charAt(0) != first_word_name.charAt(0).toUpperCase()) {
-			document.querySelector(".name-container span").remove();
-			document.querySelector(".name-container").insertAdjacentHTML("beforeend", "<span>La primera letra debe ser mayúscula</span>");
-		} else {
-			document.querySelector(".name-container span").remove();
-		}
-
-		// Validacion apellido
-		if (last_name.value.trim() === "") {
-			document.querySelector(".lastname-container").insertAdjacentHTML("beforeend", "<span>Campo obligatorio</span>");
-		} else if (!last_name.value.match(patternA_Z)) {
-			document.querySelector(".lastname-container span").remove();
-			document.querySelector(".lastname-container").insertAdjacentHTML("beforeend", "<span>Solo se permiten letras [A-Z]</span>");
-		} else if (first_word_lastname.charAt(0) != first_word_lastname.charAt(0).toUpperCase()) {
-			document.querySelector(".lastname-container span").remove();
-			document.querySelector(".lastname-container").insertAdjacentHTML("beforeend", "<span>La primera letra debe ser mayúscula</span>");
-		} else {
-			document.querySelector(".lastname-container span").remove();
-		}
-
-		// Validacion correo
-		if (email.value.trim() === "") {
-			document.querySelector(".email-container").insertAdjacentHTML("beforeend", "<span>Campo obligatorio</span>");
-		} else if (!email.value.trim().match(pattern_email)) {
-			document.querySelector(".email-container span").remove();
-			document.querySelector(".email-container").insertAdjacentHTML("beforeend", "<span>Ingrese un correo electrónico válido</span>");
-		} else {
-			document.querySelector(".email-container span").remove();
-		}
-
-		// Validacion password
-		if (password.value.trim() === "") {
-			document.querySelector(".form-group").insertAdjacentHTML("beforeend", "<span>Campo obligatorio</span>");
-		} else if (password.value.length < 6) {
-			document.querySelector(".form-group span").remove();
-			document.querySelector(".form-group").insertAdjacentHTML("beforeend", "<span>La contraseña debe tener al menos 6 caracteres</span>");
-		} else if (password.value === "password" || password.value === "123456" || password.value === "098754") {
-			document.querySelector(".form-group span").remove();
-			document.querySelector(".form-group").insertAdjacentHTML("beforeend", "<span>La contraseña es muy débil</span>");
-		} else {
-			document.querySelector("span").remove();
-		}
-
-		// Validacion select
-		if (type_bike.value === "0") {
-			document.querySelector("select").insertAdjacentHTML("afterend", "<span>Seleccione un tipo de bici</span>");
-		} else {
-			document.querySelector(".form-group select span").remove();
-		}
+	//creamos el span
+	var span = document.createElement("span");
+	//llamamos al div principal para juntarlo con nodo hijo
+	var input_box = document.getElementsByClassName("input-box");
+	//borramos los spans cada vez que se da click en registrar
+	var spans = document.getElementsByTagName("span");
+	for (var i = 0; i < spans.length; i++) {
+		spans[i].parentNode.removeChild(spans[i]);
 	}
+	// Validación nombres
+	if (name_input.length == 0) {
+		input_box[0].appendChild(span);
+		span.innerHTML = "Ingrese un nombre";
+		return false;
+	}//Sólo se permiten letras
+	else if (!name_input.match(patternA_Z)) {
+		input_box[0].appendChild(span);
+		span.innerHTML = "Solo se permiten letras de la A-Z";
+		return false;
+	}//Primera letra en mayúsucla
+	else if (name_input.charAt(0) != name_input.charAt(0).toUpperCase()) {
+		input_box[0].appendChild(span);
+		span.innerHTML = "El nombre debe comenzar con mayúsucula";
+		return false;
+	}//Validación apellidos
+	else if (last_name.length == 0) {
+		input_box[1].appendChild(span);
+		span.innerHTML = "Ingrese un apellido";
+		return false;
+	}//sólo letras en apellido
+	else if (!last_name.match(patternA_Z)) {
+		input_box[1].appendChild(span);
+		span.innerHTML = "Solo se permiten letras de la A-Z";
+		return false;
+	}//primera letra apellido mayúscula
+	else if (last_name.charAt(0) != last_name.charAt(0).toUpperCase()) {
+		input_box[1].appendChild(span);
+		span.innerHTML = "El nombre debe comenzar con mayúsucula";
+		return false;
+	} //Validación correo
+	else if (email.length == 0) {
+		input_box[2].appendChild(span);
+		span.innerHTML = "Ingrese correo electrónico";
+		return false;
+	}//email válido
+	else if (!email.match(pattern_email)) {
+		input_box[2].appendChild(span);
+		span.innerHTML = "Correo electrónico inválido";
+		return false;
+	}//contraseña
+	else if (password.length == 0) {
+		input_box[3].appendChild(span);
+		span.innerHTML = "Ingrese su contraseña";
+		return false;
+	}//contraseña de al menos 6 caracteres
+	else if (password.length < 6) {
+		input_box[3].appendChild(span);
+		span.innerHTML = "La contraseña debe tener al menos 6 caracteres";
+		return false;
+	}//password no igual a "password" ó "123456" ó "098754"
+	else if ((password == "password") || (password == "123456") || (password == "098754")) {
+		input_box[3].appendChild(span);
+		span.innerHTML = "La contraseña es muy débil";
+		return false;
+	}//opción bicis válida
+	else if (type_bike === '0') {
+		input_box[4].appendChild(span);
+		span.innerHTML = "Seleccione un tipo de bici";
+		return false;
+	}
+	return true;
+}
